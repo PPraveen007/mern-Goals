e9753f15836f7c28974c9bd2ba524e46617ac31a
Praveen Raj <rajp03262@gmail.com>
2024-01-07 23:39:37 +0530

    deploymainCode

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
+const path = require("path");
 const express = require("express");
-const colors = require("colors"); 
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorhandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
@@ -15,8 +16,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
-//useing own error handler and writhng it here make it over write it to other inbuild error         
-app.use(errorhandler);
+// Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
+//useing own error handler and writhng it here make it over write it to other inbuild error
+app.use(errorhandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
